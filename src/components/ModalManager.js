c8498080a5157ce5f03aabba8d6cdfe300f8bc0a
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-09-15 14:48:23 -0300

    Add AddRoom modal

--- src/components/ModalManager.js
+++ src/components/ModalManager.js
@@ -3,18 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Modal, Backdrop, Fade } from "@material-ui/core";
 import { useDispatch, useSelector } from "react-redux";
 import { hideModal } from "../redux/modalSlice";
+import AddRoom from "./Modals/AddRoom";
 
 const useStyles = makeStyles((theme) => ({
   modal: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
   },
 }));
 
@@ -40,12 +35,7 @@ const ModalManager = (props) => {
       }}
     >
       <Fade in={open}>
-        <div className={classes.paper}>
-          <h2 id="transition-modal-title">Transition modal</h2>
-          <p id="transition-modal-description">
-            react-transition-group animates me.
-          </p>
-        </div>
+        <AddRoom />
       </Fade>
     </Modal>
   );
