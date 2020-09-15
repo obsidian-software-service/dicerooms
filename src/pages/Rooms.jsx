a0dc2b573770f3e8bd7c0bcf12a2d104bebce055
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-09-15 14:10:22 -0300

    Rooms layout

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,5 +1,65 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper, TextField, Button, Typography } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 
-const Rooms = () => <h1>Rooms</h1>;
+import Container from "../components/Container";
+
+const styles = makeStyles({
+  root: {
+    height: "90%",
+  },
+  roomsContainer: {
+    flex: 1,
+  },
+  rooms: {
+    height: "100%",
+  },
+});
+const background =
+  "radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)";
+
+export function Rooms(props) {
+  const classes = styles(props);
+
+  return (
+    <Container background={background} transparent>
+      <Grid
+        container
+        xs={12}
+        spacing={2}
+        className={classes.root}
+        direction="column"
+      >
+        <Grid item container spacing={2}>
+          <Grid item xs={8}>
+            <Paper elevation={2}>
+              <Typography variant="h4">Salas</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper elevation={2}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                endIcon={<AddIcon />}
+                size="large"
+                fullWidth
+              >
+                Agregar Sala
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.roomsContainer}>
+          <Paper elevation={2} className={classes.rooms}>
+            Rooms
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
 
 export default Rooms;
