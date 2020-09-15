256fc0e13d1ac17af4e3e5835b537aedff2dd363
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-09-15 19:26:10 -0300

    Connect chat to firestore

--- src/pages/PlayRoom.jsx
+++ src/pages/PlayRoom.jsx
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Paper, TextField, Button, Typography } from "@material-ui/core";
 import SendIcon from "@material-ui/icons/Send";
 import Container from "../components/Container";
+import firebase from "firebase/app";
 import db from "../config/dbFirebase";
 
 const styles = makeStyles({
@@ -12,9 +13,11 @@ const styles = makeStyles({
   },
   chatContainer: {
     flex: 1,
+    overflow: "auto",
   },
   chat: {
     height: "100%",
+    overflow: "auto",
   },
 });
 const background =
@@ -22,6 +25,8 @@ const background =
 
 export function PlayRoom(props) {
   const [room, setRoom] = useState({});
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   const { id } = useParams();
   const classes = styles(props);
@@ -36,7 +41,37 @@ export function PlayRoom(props) {
           ...doc.data(),
         });
       });
+    db.collection("rooms")
+      .doc(id)
+      .collection("messages")
+      .orderBy("created")
+      .onSnapshot((querySnapshot) => {
+        const auxMessages = [];
+        querySnapshot.forEach((doc) => {
+          auxMessages.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+        setMessages(auxMessages);
+      });
   }, []);
+
+  const handleSubmitMessage = (e) => {
+    e.preventDefault();
+    db.collection("rooms")
+      .doc(id)
+      .collection("messages")
+      .add({
+        content: message,
+        user: {
+          id: 0,
+          name: "Test1",
+        },
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+    setMessage("");
+  };
 
   return (
     <Container background={background} transparent>
@@ -61,7 +96,11 @@ export function PlayRoom(props) {
         </Grid>
         <Grid item className={classes.chatContainer}>
           <Paper elevation={2} className={classes.chat}>
-            Chat
+            {messages.map((msg) => (
+              <Typography>
+                {msg.user.name}:{msg.content}
+              </Typography>
+            ))}
           </Paper>
         </Grid>
         <Grid item container direction="column" spacing={1}>
@@ -69,39 +108,46 @@ export function PlayRoom(props) {
             <Paper elevation={2}>Botonera</Paper>
           </Grid>
           <Grid item>
-            <Paper elevation={2}>
-              <Grid
-                item
-                container
-                xs={12}
-                direction="row"
-                justify="space-between"
-                spacing={1}
-                alignItems="center"
-              >
-                <Grid item xs={10}>
-                  <TextField
-                    id="chatInput"
-                    label="Chat"
-                    variant="outlined"
-                    size="small"
-                    fullWidth
-                  />
+            <form onSubmit={handleSubmitMessage}>
+              <Paper elevation={2}>
+                <Grid
+                  item
+                  container
+                  xs={12}
+                  direction="row"
+                  justify="space-between"
+                  spacing={1}
+                  alignItems="center"
+                >
+                  <Grid item xs={10}>
+                    <TextField
+                      id="chatInput"
+                      label="Mensaje"
+                      variant="outlined"
+                      size="small"
+                      fullWidth
+                      multiline
+                      rows={2}
+                      onChange={(e) => setMessage(e.target.value)}
+                      value={message}
+                    />
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className={classes.button}
+                      endIcon={<SendIcon />}
+                      size="large"
+                      fullWidth
+                      type="submit"
+                    >
+                      Enviar
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid item xs={2}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}
-                    endIcon={<SendIcon />}
-                    size="large"
-                    fullWidth
-                  >
-                    Enviar
-                  </Button>
-                </Grid>
-              </Grid>
-            </Paper>
+              </Paper>
+            </form>
           </Grid>
         </Grid>
       </Grid>
