c0b692140383966d179bee56e2c860237dce6518
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-09-15 21:23:48 -0300

    Add google auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import firebase from "firebase/app";
+import db from "../config/dbFirebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-const Login = () => <h1>Login</h1>;
+const Login = () => {
+  const uiConfig = {
+    signInFlow: "popup",
+    signInSuccessUrl: "/rooms",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  };
+
+  return (
+    <>
+      <h1>Ingresa</h1>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </>
+  );
+};
 
 export default Login;
