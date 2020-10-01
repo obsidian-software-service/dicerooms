82f71629525fcae001cb7de5802a5d501277f2d7
Roger Coverzola <diolink.4103@gmail.com>
2020-10-01 15:24:25 -0300

    Firebase config

--- src/config/dbFirebase.js
+++ src/config/dbFirebase.js
@@ -4,13 +4,14 @@ import "firebase/auth";
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "dicerooms.firebaseapp.com",
-  databaseURL: "https://dicerooms.firebaseio.com",
-  projectId: "dicerooms",
-  storageBucket: "dicerooms.appspot.com",
+  authDomain: "dicerooms-e42dd.firebaseapp.com",
+  databaseURL: "https://dicerooms-e42dd.firebaseio.com",
+  projectId: "dicerooms-e42dd",
+  storageBucket: "dicerooms-e42dd.appspot.com",
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 
 firebase.initializeApp(config);
 
